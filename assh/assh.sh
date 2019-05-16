33d2ec797f19e933ad42b0f808631b05ba9439b9
Francis Sabado <fsabado@sandia.gov>
2019-05-16 17:04:10 -0600

    Added more snippets on CEE resources and added advanched ssh

---
+++ assh/assh.sh
@@ -0,0 +1,22 @@
+#Configuration settings for assh.sh
+#echo "Setting up assh"
+
+#Get current path
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+SCRIPTHOME=$DIR
+
+#Set path variables here
+export PATH=$SCRIPTHOME/bin:$PATH
+export LD_LIBRARY_PATH=$SCRIPTHOME/lib:$LD_LIBRARY_PATH
+
+#Cleanup
+unset SOURCE
+unset DIR
+unset SCRIPTHOME
