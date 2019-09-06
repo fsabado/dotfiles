ebf60d0ea845ae866093c310b750ae2fc45a40f8
Francis Sabado <fsabado@sandia.gov>
2019-09-06 03:52:28 +0000

    Changed Assh settings

--- assh/config.sh
+++ assh/config.sh
@@ -1,6 +1,12 @@
-#Configuration settings for assh.sh
-#echo "Setting up assh"
+#/usr/bin/env bash
 
+#Script Pre-setup
+#Print bash commands
+#https://stackoverflow.com/questions/5750450/how-can-i-print-each-command-before-executing
+#https://wiki.bash-hackers.org/scripting/debuggingtips#use_shell_debug_output
+if [ ! -z ${TRACE+x} ]; then 
+	set -o xtrace 
+fi
 #Get current path
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -12,6 +18,16 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 SCRIPTHOME=$DIR
 
+#########MAIN########
+
+PROGRAM_NAME=AdvancedSSH
+#Configuration settings
+#echo "Setting up ${PROGRAM_NAME}"
+
+#Shortcut for editing this file
+ASSHRC=${SOURCE}
+alias edit-asshrc="${EDITOR} ${ASSHRC}"
+
 #Set path variables here
 export PATH=$SCRIPTHOME/bin:$PATH
 export LD_LIBRARY_PATH=$SCRIPTHOME/lib:$LD_LIBRARY_PATH
@@ -20,3 +36,10 @@ export LD_LIBRARY_PATH=$SCRIPTHOME/lib:$LD_LIBRARY_PATH
 unset SOURCE
 unset DIR
 unset SCRIPTHOME
+unset PROGRAM_NAME
+set +o xtrace
+
+
+
+
+
