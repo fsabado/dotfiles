1073484701b1b013923268369f16403c500e7a68
Francis Sabado <fsabado@sandia.gov>
2019-10-17 17:40:40 -0600

    [Bash] Fixed incorrect editor config. Added script template.

--- bash/configs/editor.sh
+++ bash/configs/editor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check if editor exist, these will be replaced in best options
-command -v gedit >/dev/null 2>&1 && export EDITOR=vim; #1st choice
+command -v vim >/dev/null 2>&1 && export EDITOR=vim; #1st choice
 command -v gedit >/dev/null 2>&1 && export EDITOR=gedit; #2nd choice
 command -v atom >/dev/null 2>&1 && export EDITOR=atom; #3rd choice
-command -v gedit >/dev/null 2>&1 && export EDITOR=sublime_text; #4th choice
+command -v sublime_text >/dev/null 2>&1 && export EDITOR=sublime_text; #4th choice
 
 alias edit="${EDITOR}"
 #Example: edit <filename>
