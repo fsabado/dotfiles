64e48c794663082abd02e5d236fe5340ac62a8a1
fsabado <fs8080@gmail.com>
2022-06-26 05:51:49 -0600

    Initial nvim init.lua. Added in the installation yaml.

--- nvim/config/nvim/init.lua
+++ nvim/config/nvim/init.lua
@@ -14,15 +14,15 @@ vim.opt.cursorline = true
 -- global scope
 vim.opt.autowrite = true
 
+local set = vim.opt
 -- Set the behavior of tab
-set.tabstop = 4
-set.shiftwidth = 4
-set.softtabstop = 4
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
 set.expandtab = true
 
 -- Cursor Lines
 vim.opt.cursorline = true
-
 
 -- Guide: https://www.youtube.com/watch?v=QB9V__3VO2s
 -- Init Vim Plug
@@ -37,14 +37,14 @@ Plug 'tpope/vim-repeat'
 
 -- The NERDTree is a file system explorer for the Vim editor
 -- https://github.com/preservim/nerdtree
-Plug 'scrooloose/nerdtree', {'on': 'NERDTreeToggle'}
+Plug 'scrooloose/nerdtree'
 
 -- Yet Another Remote Plugin Framework for Neovim
 -- https://github.com/roxma/nvim-yarp
-Plug 'roxma/nvim-yarp', { 'do': 'pip install -r requirements.txt' }
+Plug 'roxma/nvim-yarp'
 -- "dark powered neo-completion": Code completion
 -- https://github.com/Shougo/deoplete.nvim
-Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'Shougo/deoplete.nvim'
 
 -- Brings JetBrains/IntelliJ IDEs magic to Neovim with minimal setup.
 -- https://github.com/beeender/Comrade
