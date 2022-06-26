1d5c3de0352ddb06cdbb599a73f67e41d9ee3f75
fsabado <fs8080@gmail.com>
2022-06-26 04:00:08 -0600

    Fixed ssh-agent start function.

--- bash/configs/functions.sh
+++ bash/configs/functions.sh
@@ -326,40 +326,11 @@ f_systemInfo() {
 
 # Start the ssh-agent if needed
 f_start_agent() {
-#    local SSH_ENV="$HOME/.ssh/agent-environment"
-#
-#    # Check if the SSH_ENV already exist
-#    if [ -f "${SSH_ENV}" ]; then
-#        . "${SSH_ENV}" > /dev/null
-#        #Check if the agent is already started. If not, start it
-#        ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-#            start_agent;
-#        }
-#    else
-#        #The settings do not exist
-#        start_agent;
-#    fi
-#
-#
-#    function start_agent {
-#        echo "Initialising new SSH agent..."
-#        /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-#        echo succeeded
-#        chmod 600 "${SSH_ENV}"
-#        . "${SSH_ENV}" > /dev/null
-#        /usr/bin/ssh-add;
-#    }
-#
-#    # Source SSH settings, if applicable
-#
-#    if [ -f "${SSH_ENV}" ]; then
-#        . "${SSH_ENV}" > /dev/null
-#        #ps ${SSH_AGENT_PID} doesn't work under cywgin
-#        ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-#            start_agent;
-#        }
-#    else
-#        start_agent;
-#    fi
-    echo "disabled"
+    if ps -p ${SSH_AGENT_PID} > /dev/null
+    then
+        echo "ssh-agent is already running"
+    else
+        eval `ssh-agent -s`
+    fi
+
 }
