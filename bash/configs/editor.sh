65f26375a725613e77799b29f330225a2893a5eb
fsabado <fs8080@gmail.com>
2022-07-11 12:27:18 -0500

    * Added nvim as one of the main editors.

--- bash/configs/editor.sh
+++ bash/configs/editor.sh
@@ -2,6 +2,7 @@
 
 # Check if editor exist, these will be replaced in best options
 command -v vim >/dev/null 2>&1 && export EDITOR=vim; #1st choice
+command -v nvim >/dev/null 2>&1 && export EDITOR=nvim; #1st choice
 command -v gedit >/dev/null 2>&1 && export EDITOR=gedit; #2nd choice
 command -v atom >/dev/null 2>&1 && export EDITOR=atom; #3rd choice
 command -v sublime_text >/dev/null 2>&1 && export EDITOR=sublime_text; #4th choice
