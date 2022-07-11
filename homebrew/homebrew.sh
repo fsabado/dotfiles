d92c917c7a2ad18b1d11dcc255d4debae1dcd673
Francis Sabado <fs8080@gmail.com>
2022-07-11 18:14:59 -0500

    * Added run functions for homebrew.

--- homebrew/homebrew.sh
+++ homebrew/homebrew.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 export PATH=${PATH}:/home/linuxbrew/.linuxbrew/bin
+
+
+function f_brew_bootstrap_mac (){
+    brew bundle --file=~/dotfiles/homebrew/Brewfile-mac
+}
+
+function f_brew_backup_mac (){
+    brew bundle dump --file=~/dotfiles/homebrew/Brewfile-mac
+}
+
+function f_brew_bootstrap_linux (){
+    brew bundle --file=~/dotfiles/homebrew/Brewfile-linux
+}
+
+function f_brew_backup_linux (){
+    brew bundle dump --file=~/dotfiles/homebrew/Brewfile-linux
+}
